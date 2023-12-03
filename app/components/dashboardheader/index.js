a893662efef2bdc59e3c2b62d8050ff6701c6264
Taylorgtyler <taylorgtyler@gmail.com>
2023-12-03 09:03:28 -0600

    updated home icon

--- app/components/dashboardheader/index.js
+++ app/components/dashboardheader/index.js
@@ -1,7 +1,12 @@
 import React from "react";
+import { HomeIcon } from "@heroicons/react/24/outline";
 
 const DashboardHeader = () => {
-  return <div className="bg-white p-6"></div>;
+  return (
+    <div className="bg-white p-4 z-50 rounded-bl-md rounded-br-md flex justify-end">
+      <HomeIcon className="h-6 w-6 text-blue-950 mr-4" />
+    </div>
+  );
 };
 
 export default DashboardHeader;
